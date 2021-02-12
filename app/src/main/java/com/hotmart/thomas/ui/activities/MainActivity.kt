8cb28ff911373eddef94c796d180a428acd1cf61
thomas <thomas.marquesbr@gmail.com>
2021-02-12 00:18:54 -0300

    Adicionar splash screen

--- app/src/main/java/com/hotmart/thomas/ui/activities/MainActivity.kt
+++ app/src/main/java/com/hotmart/thomas/ui/activities/MainActivity.kt
@@ -7,6 +7,7 @@ import com.hotmart.thomas.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_HotmartMobileApp)
         setContentView(R.layout.activity_main)
     }
 }
